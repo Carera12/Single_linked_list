5641c9e0653a09a719e5655344ad5805248802d9
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-11-07 16:48:30 +0700

    Add project files.

---
+++ Single_linked_list/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_linked_list
+{
+    // each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+
+    class List
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
