3503ec8dc24e336673c55d04dfc152dd8cb7e0a0
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-11-07 17:00:27 +0700

    add public void addnote() to add a node in the list

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -23,6 +23,18 @@ namespace Single_linked_list
         {
             START = null;
         }
+        public void addNote() //add a node in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the studenet: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+        }
     }
     class Program
     {
