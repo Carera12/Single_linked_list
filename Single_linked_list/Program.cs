72f362a18b7e255fd794bf0e931c731f01e0486e
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-11-07 18:08:41 +0700

    add while try to text option menu

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -118,6 +118,25 @@ namespace Single_linked_list
         static void Main(string[] args)
         {
             List obj = new List();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all the records in the lisy");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("\nEnter your choice (1-5) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("\nCheck for the value enterd");
+                }
+            }
         }
     }
 }
